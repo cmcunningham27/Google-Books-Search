743773137b73ebd38a073b677d4985f8b6784064
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-06-26 15:05:42 -0700

    imported Nav component and created it in App.js, created index.js file in components/Nav folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
 import Saved from './pages/Saved';
 import Search from './pages/Search';
 import "./App.css";
@@ -7,6 +8,7 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route exact path='/saved'>
           {Saved}

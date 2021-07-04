6adea2b3883874b03fc86a9219e951d60c56704c
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-04 11:46:02 -0700

    changes Save component to a state class component, created default state, and added componentDidMount function to make API request and set the books array with the returned data

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import API from '../utils/API';
 
-export default function Saved() {
-    return (
-        <div>
-            <h1>This is Saved</h1>
-        </div>
-    )
+class Saved extends Component {
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
+        API.getSavedBooks()
+            .then((res) => {
+                this.setState({
+                    books: res.data.results.map((e, i) => ({
+                        key: i,
+                        title: e.volumeInfo.title,
+                        authors: e.columeInfo.authors.join(', '),
+                        description: e.volumeInfo.description,
+                        image: e.volumeInfo.imageLinks.thumbnail,
+                        link: e.volumeInfo.infoLink
+                    }))
+                })
+            })
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>This is Saved</h1>
+            </div>
+        )
+    }  
 }
+
+export default Saved;

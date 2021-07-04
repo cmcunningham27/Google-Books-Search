b62bf94c49bc7dac0a455cca554f7d9c178d274d
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-04 12:24:14 -0700

    added code for deleting specific book from database in booksController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -14,5 +14,13 @@ module.exports = {
             .find({})
             .then(dbModel => res.json(dbModel))
             .catch(err => console.log(err));
+    },
+
+    remove: function(req, res) {
+        db.Book
+            .findById({_id: req.params.id})
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => console.log(err));
     }
 }

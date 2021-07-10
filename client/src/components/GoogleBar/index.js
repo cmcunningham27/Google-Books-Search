220dcc5320feea17245a1712aff6dba1e982da0c
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-10 13:14:11 -0700

    styled GoogleBar component

--- client/src/components/GoogleBar/index.js
+++ client/src/components/GoogleBar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './style.css';
 
 const GoogleBar = ({handleSearch, handleBookSearch, searchTerm}) => {
 
     return (
-        <div>
-            <h6>Book Search</h6>
-            <p>Book</p>
+        <div className='searchBar'>
+            <h5>Book Search</h5>
+            <h6>Book</h6>
             <input type='text' className='' placeholder='Search For a Book' onInput={handleSearch}/> 
             <button name='search' className='search' onClick={handleBookSearch}>
                 Search

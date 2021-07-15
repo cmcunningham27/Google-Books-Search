ab8491c3554d2095d9be77cf06d6939fc3d2ec3a
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-15 14:53:46 -0700

    comments added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bookRoutes = require('./books');
 
+//adds /books before each request in books api routes
 router.use('/books', bookRoutes);
 
 module.exports = router;

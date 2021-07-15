80a492006e8b0dbdf4493b6380533afc25e57780
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-15 14:58:37 -0700

    comment added

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-
+//attaches React app in index.html at the root
 ReactDOM.render(<App />, document.getElementById("root"));
-
--- controllers/googleController.js
+++
@@ -1,8 +0,0 @@
-const db = require('../models');
-
-module.exports = {
-    searchBooks: function(req, res) {
-        axios.get('https://www.googleapis.com/books/v1/volumes/?happy')
-            
-    }
-}

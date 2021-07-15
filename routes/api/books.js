62b604cc856e299c0563ed5df3f7890fbe81d630
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-15 14:52:18 -0700

    comments added

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,17 +1,17 @@
 const router = require('express').Router();
 const booksController = require('../../controllers/booksController');
 
+//adds a book to the database
 router.route('/')
     .post(booksController.create)
-    // .post((req, res) => {
-    //     res.send('Hello');
-    // })
-    
+  
+//gets all saved books from database
 router.route('/saved')
     .get(booksController.findAll)
 
+//removes a book from the database based on its id
 router.route('/:id')
     .delete(booksController.remove)
     
-
+//exports all routes to be utilized elsewhere
 module.exports = router;

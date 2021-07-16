6ecfc1f1457d4730696be64cf810a54b188b78b3
Cassandra Cunningham <cmcunningham27@gmail.com>
2021-07-15 17:56:34 -0700

    added media queries for elements in the GoogleBar component

--- client/src/components/GoogleBar/index.js
+++ client/src/components/GoogleBar/index.js
@@ -8,7 +8,7 @@ const GoogleBar = ({handleSearch, handleBookSearch}) => {
             <h5 className='bookSearch'>Book Search</h5>
             <h6 className='book'>Book</h6>
             {/* runs function on input */}
-            <input type='text' className='' placeholder='Search For a Book' onInput={handleSearch}/> 
+            <input type='text' placeholder='Search For a Book' onInput={handleSearch}/> 
             {/* runs function when button is searched */}
             <button name='search' className='search' onClick={handleBookSearch}>
                 Search
